ba40c85fbd14faf00d3c62c3c673ea9a826f816b
Sergey Lihobabin <sergey.lihobabin@gmail.com>
2024-10-31 21:32:20 +0500

    fix s3 backup script

--- django_nextjs_template/backend/s3_backup.py
+++ django_nextjs_template/backend/s3_backup.py
@@ -6,6 +6,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 from django.conf import settings
 
 S3_BACKUP_PATH = '/tmp/s3_backup'
+# This is the endpoint URL without the bucket name for DigitalOcean Spaces, because otherwise it will not work
+ENDPOINT_URL = settings.S3_ENDPOINT.replace(f'{settings.S3_BUCKET}.', '')
 
 def download_dir(client: Any, dir: str, local_dir: str, bucket: str):
     print(f'Downloading Directory "{dir}" from bucket "{bucket}"')
@@ -24,12 +26,12 @@ def download_dir(client: Any, dir: str, local_dir: str, bucket: str):
 
 def backup_s3():
     print('Initiating S3 Backup')
-    print(f'S3 Endpoint {settings.S3_ENDPOINT}')
+    print(f'S3 Endpoint {ENDPOINT_URL}')
     print(f'S3 Bucket {settings.S3_BUCKET}') 
     print(f'S3 Key ID {settings.S3_ACCESS_KEY_ID}')
     client = boto3.client( # type: ignore
         service_name='s3',
-        endpoint_url=settings.S3_ENDPOINT,
+        endpoint_url=ENDPOINT_URL,
         aws_access_key_id=settings.S3_ACCESS_KEY_ID,
         aws_secret_access_key=settings.S3_SECRET_KEY,
     )
