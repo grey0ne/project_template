24734aec6b4a582324a5972e454699e565ecb114
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-07-27 23:25:30 +0500

    settings simplified even more, npm update added to frontend dockerfile

--- django_nextjs_template/backend/application/settings.py
+++ django_nextjs_template/backend/application/settings.py
@@ -1,7 +1,3 @@
-
-from sentry_sdk.integrations.django import DjangoIntegration
-from dataorm.settings_helpers import config_get_str
-
 from dataorm.base_settings import *
 
 from application.project_settings import *  # type: ignore used to customize templated settings
